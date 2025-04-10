00e234366f6dcb8aaf8229f96cb5bf2591e4ada4
giulio177 <99282338+giulio177@users.noreply.github.com>
2025-04-11 01:49:12 +0200

    Update main_window.py

--- gui/main_window.py
+++ gui/main_window.py
@@ -54,7 +54,6 @@ class MainWindow(QMainWindow):
         self.base_bottom_bar_button_size = QSize(65, 65) # Larger buttons
         self.base_bottom_bar_height = 90 # Taller bottom bar
         self.base_volume_slider_width = 250 # Wider slider
-        self.base_volume_slider_thickness = 18 # <<< --- TWEAK THIS VALUE --- >>>
         self.base_layout_spacing = 15 # More spacing between widgets generally
         self.base_header_spacing = 20 # More spacing between header items
         self.base_layout_margin = 8 # Bottom bar internal margin
@@ -355,7 +354,7 @@ class MainWindow(QMainWindow):
         self.bottom_bar_layout.setSpacing(scaled_spacing)
 
         # --- Re-apply theme/stylesheet ---
-        apply_theme(QApplication.instance(), self.current_theme, scale_factor, self.base_volume_slider_thickness)
+        apply_theme(QApplication.instance(), self.current_theme, scale_factor)
 
         # --- Update Header Bluetooth Status ---
         self.update_bluetooth_header_status() # Update text/visibility
