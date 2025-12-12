1cad95070709a92dd1c3ce2298677a3fe82e78f7
giulio177 <99282338+giulio177@users.noreply.github.com>
2025-12-12 11:20:52 +0100

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,4 +4,4 @@ sudo apt update
 sudo apt install -y python3-pygame libsdl2-2.0-0 libsdl2-image-2.0-0 libsdl2-ttf-2.0-0
 
 echo "Avvio Installer Grafico Touch..."
-sudo python3 installer_gui.py
+sudo python3 scripts/installer_gui.py
